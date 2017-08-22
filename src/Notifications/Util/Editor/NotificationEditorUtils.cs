3dc0c1d0002945a2278fe9b07547790a5ac354b3
larrykirschner <larrykirschner@gmail.com>
2017-08-21 17:52:20 -0700

    0.0.5

--- src/Notifications/Util/Editor/NotificationEditorUtils.cs
+++ src/Notifications/Util/Editor/NotificationEditorUtils.cs
@@ -1,42 +1,10 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-//using BeatThat.Editor;
-
-namespace BeatThat.App
+﻿namespace BeatThat.App
 {
 	public static class NotificationEditorUtils 
 	{
 		public static string[] GetAllNotificationTypes()
 		{
-			return EditorUtils.FindStaticValsWithAttrAndValType<NotificationTypeAttribute, string>();
-
-//			if(m_notificationTypes == null) {
-//				var noticationTypes = new Dictionary<string, string>();
-//				foreach(Assembly a in AppDomain.CurrentDomain.GetAssemblies()) {
-//					foreach(Type t in a.GetTypes()) {
-//						foreach(FieldInfo f in t.GetFields()) {
-//							if(f.FieldType == typeof(string)) {
-//								foreach(var attr in f.GetCustomAttributes(false)) {
-//									if(attr is NotificationTypeAttribute) {
-//										string s = f.GetRawConstantValue().ToString();
-//										noticationTypes[s] = s;
-//									}
-//								}
-//							}
-//						}
-//					}
-//				}
-//
-//				m_notificationTypes = new List<string>(noticationTypes.Keys).ToArray();
-//
-//				System.Array.Sort(m_notificationTypes);
-//			}
-//			return m_notificationTypes;
+			return TypeUtils.FindStaticValsWithAttrAndValType<NotificationTypeAttribute, string>();
 		}
-
-//		private static string[] m_notificationTypes;
 	}
 }
