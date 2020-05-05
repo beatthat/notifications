02a1de2b83b7a2052e157c93edd133e5ca82ca6a
larry kirschner <larrykirschner@gmail.com>
2020-05-05 09:36:49 -0700

    Adds Notificaton::ResolveAsync, useful for test setups
    - feature: Notification::ResolveAsync
    - improvement: Unity2019.3.12 for test package
    - improvement: more details in README

--- README.md
+++ README.md
@@ -1,1 +1,187 @@
-Flexible global pub/sub notification system
+# <a name="readme"></a>notifications
+
+Notifications are a flexible, global pub/sub message system.
+
+## Install
+
+From your unity project folder:
+
+```bash
+npm init --force && \
+npm install beatthat/notifications
+```
+
+The package and all its dependencies will be installed under Assets/Plugins/packages.
+
+In case it helps, a quick video of the above: https://youtu.be/Uss_yOiLNw8
+
+## USAGE
+
+If you're using this notifications package, you probably encountered it through other `beatthat` unity3d packages that leverage notifications and also simplify their use. Will give some examples of those packages further down, but for now if you were using Notifications without any other support it would look like this:
+
+```csharp
+using BeatThat.Notifications;
+
+public static class StatusNotifications
+{
+    /// Notifications are identified by string types
+    /// Usually, you want to define those types as constants somewhere
+    public const string STATUS_UPDATED = "STATUS_UPDATED";
+}
+
+public class StatusObserver : MonoBehavior
+{
+    void Start()
+    {
+        NotificationBus.Add<string>(StatusNotifications.STATUS_UPDATED, this.OnStatusUpdated);
+    }
+
+    void OnStatusUpdated(string newStatus)
+    {
+        Debug.Log("got new status: " + newStatus);
+    }
+}
+
+public class StatusPublisher : MonoBehavior
+{
+    public string status;
+
+    public void SetStatus(string newStatus) 
+    {
+        this.status = d;
+        NotificationsBus.Send(StatusNotifications.STATUS_UPDATED, newStatus)
+    }
+}
+```
+
+The example above is very contrived but the main idea is that `StatusObserver` can get status updates without needing to know anything about `StatusPublisher`. Instead they both depend on shared notification type.
+
+## A Practical Example: State Stores
+
+The example above is really a simplication of State Stores, which are a common use case for notificitions. The basic is to have a global singleton that manages some state item and then observers of that state that can both access the state value and subscribe to on-update notifications.
+
+A more usable version of the status-update example can be built using the [state-stores](https://github.com/beatthat/state-stores) package (in concert with a few other packages I will detail below)
+
+```csharp
+/// pretend we have a slightly more complex 
+/// StatusData struct for our state
+public struct StatusData
+{
+    public string status;
+    public bolean isHappy;
+}
+
+using BeatThat.Service;
+using BeatThat.StateStores;
+// register this singleton as implementation 
+// of interface HasState<StatusData> (see beatthat/services below)
+[RegisterService(HasState<StatusData>)]
+public class StatusStore : StateStore<StatusData> 
+{
+    // Will expose the StatusData state as property `stateData`
+    // Not shown how here, but just assume that state can be updated
+}
+
+
+using BeatThat.Controllers;
+using BeatThat.DependencyInjection;
+using BeatThat.StateStores;
+public class StatusObserver : Controller 
+/// extending Controller here mainly gives support for dependency-injection 
+/// and simplified notificaton binding
+{
+    override protected void GoController() // called after dependencies injected
+    {
+        /// Bind is a wrapper for NotificationBus.Add 
+        /// that handles cleanup, i.e. it makes sure 
+        /// the registered callback is removed when this controller goes away
+        Bind(State<StatusData>.UPDATED, this.OnStatusUpdated);
+    }
+
+    void OnStatusUpdated()
+    {
+        StatusData s = this.statusStore.stateData; // t
+        Debug.Log("got new status: " + s.status + " with isHappy " + s.isHappy);
+    }
+
+    // dependency injection will set this property to our singleton service
+    // based upon matching the registered interface HasState<StatusData>
+    [Inject] HasState<StatusData> statusStore { get; set; }
+}
+
+```
+
+For more details on the packages used above see:
+
+
+#### The [service](https://github.com/beatthat/service) package manages a container of global singleton services
+
+The `[RegisterService]` attribute above triggers the creation of a global singleton registered for lookup by interface `HasState<StatusData>`
+
+
+#### The [dependency-injection](https://github.com/beatthat/dependency-injection) package assigns references to service singletons
+
+The `[Inject]` causes that property to be assigned with the registered `HasState<StatusData>` singleton
+
+#### The [controllers](https://github.com/beatthat/controllers) package simplifies use of dependency injection and notifications
+
+Extending `Controller` gives out-of-box support for Dependency Injection and simplified notificaton binding. It isn't necessary to extend controller though. For example, you could alternatively enable dependency injection on a plain MonoBehavior like this:
+
+```csharp
+void Start()
+{
+    // Something needs to call DependencyInjection.InjectDependencies.
+    // The Controller base class would have done this for you
+    BeatThat.DependencyInjection.InjectDependencies.On(this);
+}
+```
+
+#### Making sure to cleanup/unregister notification listeners
+
+If you're listening for notifications from a class that doesn't live forever0--e.g. a screen--it's important to always unregister any listeners attached to the `NotificationBus`. For example, if you have a screen that's listening for those `StatusData` notifications above, and then the user exits/destroys that screen, it will cause errors and memory leaks if the (now zombie/destroyed) screen continues receiving notifications. 
+
+This is another reason we use that `Controller` base class above. Because it has a `Bind(string notificationType, System.Action callback)` function that takes care of that cleanup for you.
+
+If you were making a screen from raw MonoBehavior and wanted to implement register + cleanup of noticiations properly, it would look like this:
+
+```csharp
+using BeatThat.Notifications;
+class MyScreen : MonoBehavior
+{
+    private NotificationBinding binding;
+    void OnEnable() {
+        this.binding = NotificationBus.Add("somenotification", this.OnNotification);
+    }
+
+    void OnNotification() {
+        // do whatever
+    }
+
+    void OnDisable() {
+        if(this.binding != null) {
+            this.binding.Unbind();
+            this.binding = null;
+        }
+    }
+}
+```
+
+...again for comparison, if we used the `Controller` base class the same thing would look like this:
+```csharp
+using BeatThat.Controllers;
+using BeatThat.Notifications;
+class MyScreen : Controller
+{
+    override protected void OnGoController() {
+        // we don't need to worry about cleaning up,
+        // controller does that for us
+        Bind("somenotification", this.OnNotification);
+    }
+
+    void OnNotification() {
+        // do whatever
+    }
+}
+```
+
+
--- Runtime/notifications/Notification.cs
+++ Runtime/notifications/Notification.cs
@@ -1,3 +1,4 @@
+using BeatThat.Pools;
 using System;
 using System.Collections.Generic;
 
@@ -9,121 +10,157 @@ namespace BeatThat.Notifications
     /// Additionally, notifications may pass a single param 'body'
     /// or if a notification has muliple params it may pass key value properties.
     /// </summary>
-    public struct Notification 
-	{
-		public Notification(string type, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver) : this(type, null, opts)
-		{
-		}
+    public struct Notification
+    {
+        public Notification(string type, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver) : this(type, null, opts)
+        {
+        }
 
 
-		public Notification(string type, object body, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver) : this(type, null, body, opts) 
-		{
-		}
-			
-		public Notification(string type, string group, object body, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver)
-		{
-			this.type = type;
-			this.group = group;
-			this.body = body;
-			this.receiverOptions = opts;
-			m_properties = null;
-		}
-	
-		/// <summary>
-		/// Main id for a notification. 
-		/// Notification handlers generally register their interests by Notification type.
-		/// </summary>
-		public string type 
-		{
-			get; private set;
-		}
+        public Notification(string type, object body, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver) : this(type, null, body, opts)
+        {
+        }
 
-		/// <summary>
-		/// Generally left null, but can be used as a convenient mechanism 
-		/// to have some listeners ignore a subset of notifications for a given type.
-		/// Motivating example is window managers existing at different layers in a scene.
-		/// </summary>
-		/// <value>The group.</value>
-		public string group { get; private set; }
-		
-		/// <summary>
-		/// A convenient place to store a param for Notifications that
-		/// have just one param.
-		/// For notifications with more than one param, use properties.
-		/// </summary>
-		public object body
-		{
-			get; set;
-		}
+        public Notification(string type, string group, object body, NotificationReceiverOptions opts = NotificationReceiverOptions.RequireReceiver)
+        {
+            this.type = type;
+            this.group = group;
+            this.body = body;
+            this.receiverOptions = opts;
+            m_properties = null;
+        }
 
-		public NotificationReceiverOptions receiverOptions
-		{
-			get; set;
-		}
-		
-		public bool bodyAsBool
-		{
-			get {
-				return Convert.ToBoolean(this.body);
-			}
-		}
+        /// <summary>
+        /// Main id for a notification. 
+        /// Notification handlers generally register their interests by Notification type.
+        /// </summary>
+        public string type
+        {
+            get; private set;
+        }
 
-		public T BodyAs<T>()
-		{
-			if(this.body is System.IConvertible) { 
-				return (T)System.Convert.ChangeType(this.body, typeof(T));
-			}
-			else {
-				return (T)this.body;
-			}
-		}
+        /// <summary>
+        /// Generally left null, but can be used as a convenient mechanism 
+        /// to have some listeners ignore a subset of notifications for a given type.
+        /// Motivating example is window managers existing at different layers in a scene.
+        /// </summary>
+        /// <value>The group.</value>
+        public string group { get; private set; }
 
-		public bool TryGet<T>(string key, out T val)
+        /// <summary>
+        /// A convenient place to store a param for Notifications that
+        /// have just one param.
+        /// For notifications with more than one param, use properties.
+        /// </summary>
+        public object body
+        {
+            get; set;
+        }
+
+        public NotificationReceiverOptions receiverOptions
+        {
+            get; set;
+        }
+
+        public bool bodyAsBool
+        {
+            get
+            {
+                return Convert.ToBoolean(this.body);
+            }
+        }
+
+        public T BodyAs<T>()
+        {
+            if (this.body is System.IConvertible)
+            {
+                return (T)System.Convert.ChangeType(this.body, typeof(T));
+            }
+            else
+            {
+                return (T)this.body;
+            }
+        }
+
+        public bool TryGet<T>(string key, out T val)
+        {
+            object tmp;
+            if (m_properties != null && m_properties.TryGetValue(key, out tmp) && tmp is T)
+            {
+                val = (T)System.Convert.ChangeType(tmp, typeof(T));
+                return true;
+            }
+            else
+            {
+                val = default(T);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Set a key-value param on the Notification
+        /// </summary>
+        public Notification Set(string key, object val)
+        {
+            if (m_properties == null)
+            {
+                m_properties = new Dictionary<string, object>();
+            }
+            m_properties[key] = val;
+            return this;
+        }
+
+        /// <summary>
+        /// Enumerates all property names set on this Notification
+        /// </summary>
+        public IEnumerable<string> propertyNames
+        {
+            get
+            {
+                if (m_properties != null)
+                {
+                    foreach (string key in m_properties.Keys)
+                    {
+                        yield return key;
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Notification[type: " + this.type + ", body: " + this.body + "]";
+        }
+
+#if NET_4_6
+        /// <summary>
+        /// A delegate function that executes the request. 
+        /// MUST terminate by calling one of the passed in callback functions, resolve or reject.
+        /// This is a simpler version that doesn't provide 'cancel' or 'attach' callbacks (just for more concise code)
+        /// 
+        /// @param resolve - the outer action should call resolve with the request's result item (if the request is successful) 
+        /// 
+        /// @param reject - the outer action should call reject with an error message (if the request is a failure).
+        ///     The param type is object rather than string to enable support for secondary error info, e.g. web response codes.
+        /// 
+        /// </summary>
+        public delegate void OnNotificationDelegate<T>(Notification n, System.Threading.Tasks.TaskCompletionSource<T> reject);
+        public static async System.Threading.Tasks.Task<T> ResolveAsync<T>(string bindNotification, OnNotificationDelegate<T> onNotification, System.Action send = null)
 		{
-			object tmp;
-			if(m_properties != null && m_properties.TryGetValue(key, out tmp) && tmp is T) {
-				val = (T)System.Convert.ChangeType(tmp, typeof(T));
-				return true;
-			}
-			else {
-				val = default(T);
-				return false;
-			}
-		}
-		
-		/// <summary>
-		/// Set a key-value param on the Notification
-		/// </summary>
-		public Notification Set(string key, object val)
-		{
-			if(m_properties == null) {
-				m_properties = new Dictionary<string, object>();
-			}
-			m_properties[key] = val;
-			return this;
-		}
-		
-		/// <summary>
-		/// Enumerates all property names set on this Notification
-		/// </summary>
-		public IEnumerable<string> propertyNames
-		{
-			get {
-				if(m_properties != null) {
-					foreach(string key in m_properties.Keys) {
-						yield return key;
-					}
+			var tcs = new System.Threading.Tasks.TaskCompletionSource<T>();
+			System.Action<Notification> cb = (Notification n) => onNotification(n, tcs);
+			using(var bindings = ListPool<NotificationBinding>.Get()) {
+				bindings.Add(NotificationBus.Add(bindNotification, cb));
+				if(send != null) {
+					send();
 				}
+				return await tcs.Task;
 			}
 		}
-		
-		public override string ToString()
-		{
-			return "Notification[type: " + this.type + ", body: " + this.body + "]";
-		}
-		
-		private Dictionary<string,object> m_properties;
-	}
+#endif
+
+        private Dictionary<string, object> m_properties;
+    }
 
 }
 
